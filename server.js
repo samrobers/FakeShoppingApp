784342f3857d31eb65fdb285bb924b78e34157e3
Mona Lisa <sam.roberson32@gmail.com>
2021-05-27 21:52:09 -0600

    set up initial server

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
+const PORT = process.env.PORT || 3004;
+
+app.listen(PORT, () => {
+  console.log("This port is working");
+});
